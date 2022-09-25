d566cdd21389590b29557260eb2654022664a386
khushdj2002 <108506287+khushdj2002@users.noreply.github.com>
2022-09-25 12:25:59 +0530

    DAY 30 update

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,39 @@ const http =require("http");
 
 const port = 8081;
 
-http.createServer((request,response)=>{
-    response.writeHead(200,{"Content-Type": "text/html"});
-    response.write("<h1>Hello, this is from my server </h1>");
+const toDOList = ["Complete Node Byte", "Play Cricket"];
+
+http
+    .createServer((request,response)=>{
+    const {method,url} = request;
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            response.writeHead(200, {'Content-Type':"text/html"});
+            response.write(toDOList.toString())
+            // response.write(toDoList);
+        }
+        else if(method==="POST"){
+            let body="";
+            request.on("error",(err) => {
+                console.error(err)
+            }).on("data",(chunk)=>{
+                body+=chunk;
+           //     console.log(chunk);
+            }).on("end",()=>{
+                body =JSON.parse(body);
+                console.log("data:",body);
+            });
+        }
+        else
+        response.writeHead(404);
+    } 
+
     response.end();
-}).listen(port,()=>{
+
+})
+    .listen(port,()=>{
     console.log(`Nodejs server started on port ${port}`);
 });
 
-// http://localhost:8081
+// http://localhost:8081/todos
